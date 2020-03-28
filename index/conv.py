7108954857cac33519566f16d706eb71b3563d98
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 18:04:04 +0100

    Push Update

---
+++ index/conv.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+
+
+
+image_folder = 'output'
+video_name = 'temp.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter(video_name, fourcc, 60, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
